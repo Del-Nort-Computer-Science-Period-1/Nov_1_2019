edd8b1ee37ad0014ad67f82b1426d84d0b65fda8
colin <kolinszeto@gmail.com>
2019-10-17 09:20:42 -0700

    trying to push to GitHub

---
+++ CalcC3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CalcC3
+//
+//  Created by Szeto, Colin on 10/17/19.
+//  Copyright © 2019 Szeto, Colin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
